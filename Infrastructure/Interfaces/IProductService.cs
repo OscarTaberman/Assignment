78e52d7c949cbbb0fccfdc81fe87e60bd2a1f549
Oscar Taberman <oscartaberman@gmail.com>
2025-09-22 16:29:40 +0200

    Created IProductService and ProductResult.

---
+++ Infrastructure/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface IProductService
+{
+    ProductResult CreateProduct(Product product);
+    ProductResult<IEnumerable<Product>> ReadAllProducts();
+    ProductResult UpdateProduct(Product product);
+    ProductResult DeleteProduct(Product product);
+}
---
+++ Infrastructure/Models/ProductResult.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Infrastructure.Models;
+
+public class ProductResult
+{
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+}
+
+public class ProductResult<Type>
+{
+    public Product? Result { get; set; }
+}
